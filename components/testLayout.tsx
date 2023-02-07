db9b830eddbeacc93add4581d3c30ed722e3abf1
Filip Mannheim <filip.mannheim@gmail.com>
2023-02-07 17:53:14 +0100

    more cat

--- components/testLayout.tsx
+++ components/testLayout.tsx
@@ -10,11 +10,26 @@ import middleLeft from "../public/middleLeft.png";
 import middleRight from "../public/middleRight.png";
 import topRight from "../public/topRight.png";
 import topMiddle from "../public/topMiddle.png";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export function TestLayout() {
   // const [playPrayer1, exposedPrayer1] = useSound(prayer1, { interrupt: false });
   // const [playing, setPlaying] = useState(false);
+
+  const [selectedOption, setSelectedOption] = useState("ahoj");
+  const [activeKitten, setActiveKitten] = useState(
+    "https://placekitten.com/350/300"
+  );
+
+  useMemo(() => {
+    const a = (Math.round(Math.random() * 1024) + 32).toString(10);
+    const b = (Math.round(Math.random() * 512) + 128).toString(10);
+    setActiveKitten("https://placekitten.com/" + a + "/" + b);
+    console.log(activeKitten);
+  }, [selectedOption]);
+
+  const selectOptions: string[] = ["ahoj", "meowhoj", "hello", "meowello"];
+
   return (
     <div className={styles.wrapper}>
       {/* top */}
@@ -26,13 +41,21 @@ export function TestLayout() {
       </div>
       <div className={styles.topRightWrapper}>
         <Image src={topRight} />
+        <select
+          className={styles.samplePlaylistInput}
+          onChange={(event) => setSelectedOption(event.target.value)}
+        >
+          {selectOptions.map((item) => (
+            <option value={item}>{item}</option>
+          ))}
+        </select>
       </div>
       {/* middle */}
       <div className={styles.middleLeftWrapper}>
         <Image src={middleLeft} alt="" layout="fill" />
       </div>
       <div className={styles.contentWrapper}>
-        <Image src={"https://placekitten.com/350/300"} layout="fill" />
+        <Image src={activeKitten} layout="fill" />
       </div>
       <div className={styles.middleRightWrapper}>
         <Image src={middleRight} alt="" layout="fill" />
